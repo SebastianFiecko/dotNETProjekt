d900e885c88eec15345e44ee64ee55ae7bdca763
seba <sebastian.fiecko@gmail.com>
2017-07-24 21:10:46 +0200

    Clean-up. Configured gitignore file and deleted all the Build-relevant objects

--- DOT_NET_Project/DOT_NET_Project.Android/SplashActivity.cs
+++ DOT_NET_Project/DOT_NET_Project.Android/SplashActivity.cs
@@ -4,7 +4,7 @@ using Android.Support.V7.App;
 
 namespace DOT_NET_Project.Droid
 {
-    [Activity(Label = "Mobile App Name", Icon = "@drawable/icon", Theme = "@style/splashscreen", MainLauncher = true, NoHistory = true)]
+    [Activity(Label = "DOT_NET_Project", Icon = "@drawable/icon", Theme = "@style/splashscreen", MainLauncher = true, NoHistory = true)]
     public class SplashActivity : AppCompatActivity
     {
         protected override void OnResume()
