950f26cd969a3ca9088596d693d5d5448702be06
seba <sebastian.fiecko@gmail.com>
2017-07-24 19:48:58 +0200

    Initial commit - core application - with Layout and a SplashScreen

---
+++ DOT_NET_Project/DOT_NET_Project.Android/SplashActivity.cs
@@ -0,0 +1,16 @@
+﻿
+using Android.App;
+using Android.Support.V7.App;
+
+namespace DOT_NET_Project.Droid
+{
+    [Activity(Label = "Mobile App Name", Icon = "@drawable/icon", Theme = "@style/splashscreen", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
---
+++ DOT_NET_Project/DOT_NET_Project/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace DOT_NET_Project
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new DOT_NET_Project.MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
